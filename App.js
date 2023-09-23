457bd0ee317d4fa25da599ece3cfd525f0c2676c
t1kyje00@students.oamk.fi <t1kyje00@students.oamk.fi>
2023-09-23 15:02:43 +0300

    With uploaded italic font

--- App.js
+++ App.js
@@ -1,9 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [loaded] = useFonts({
+    //LatoRegular: require('./assets/fonts/Lato-Regular.ttf'),
+    LatoItalic: require('./assets/fonts/Lato-Italic.ttf')
+  })
+
+if(!loaded) {
+  return null;
+}
+
   return (
     <View style={styles.container}>
+      <Text style={styles.heading}>My heading</Text>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -17,4 +28,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  heading: {
+    //fontFamily: 'Latoregular',
+    fontFamily: 'LatoItalic',
+    fontSize: 40,
+  }
 });
